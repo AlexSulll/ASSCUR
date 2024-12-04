98a8464023470342f7ce8a59dabde56e3cc31bdb
AlexSulll <hlebusheck.tv@gmail.com>
2024-12-04 20:51:14 +0300

    full adc comfile

--- COD.asm
+++ COD.asm
@@ -6,6 +6,9 @@
     read_file       db      'COM.COM',0
     com_file        db      'result.asm',0
     Peremenaya_adc  db      9,'adc',9,'$'
+    reg1            db      ' ,$'
+    reg2            db      ' $'
+    two_bite_prefix db      '0F $'
 .code
 Start:
     mov     ax,@data
@@ -16,5 +19,43 @@ Start:
     mov     ax,3D02h
     int     21h
     
-    end     Start
+    mov     bx,ax
+    mov     ax,3F00h
+    mov     cx,1024d
+    mov     dx,offset buffer
+    int     21h
+    
+    mov     ax,3E00h
+    mov     bx,5
+    int     21h
+    
+    mov     si,offset buffer
+    
+    mov     dx,offset com_file
+    mov     ax,3C00h
+    mov     cx,2
+    int     21h
+    
+    jmp     nachalo
+zapis:
+    mov     ah,40h
+    mov     bx,5
+    int     21h     
+
+    mov     ah,09h
+    int     21h
+    ret
+nachalo:
+    mov     si,offset buffer
+    lodsb
+    cmp     al,10h
+    jz      opc10
+    
+opc10:
+    lodsb
+    
+Exit:
+    mov     ax,4C00h
+    int     21h
+    end     Start 
     
--- COM.asm
+++ COM.asm
@@ -1,7 +1,5 @@
 .model tiny
 .386
-.data
-    A   db    10
 .code
 org     100h
 Start:
@@ -14,7 +12,10 @@ Start:
     adc10   0F1h
     adc10   0F5h
     adc10   0D5h
+    repe lock adc     ds:[bx+si],ah
     adc     ds:[bx+si],ah
+    adc     ds:[bx+si+11h],ah
+    adc     ds:[bx+si+1111h],ah
     
     adc11   macro op;macros 11h + oper
     db      11h,op
@@ -24,6 +25,7 @@ Start:
     adc11   0C1h
     adc11   0D1h
     adc11   0D9h
+    
     adce11  macro op ;macro 66h + adc11 = 66h+11h+oper
     db      66h
     adc11   op
@@ -34,28 +36,78 @@ Start:
     adce11  0D1h
     adce11  0D9h
     adc     ds:[bx+si],ax
-    adc     ds:[ebx+esi],eax
+    adc     es:[bx+si],ax
+    adc     ds:[bx+si+11h],ax
+    adc     ds:[bx+si+1111h],ax
+    adc     fs:[ebx+esi],eax
+    adc     es:[ebx+esi+11h],eax
+    adc     gs:[ebx+esi+1111h],eax
+    adc     ss:[ebx+esi+11111111h],eax
     
+    adc     ah,cl;12 op
+    adc     al,dh
+    adc     dl,ch
+    adc     ch,ds:[bx+si]
+    adc     ch,ds:[bx+si+11h]
+    adc     ch,ds:[bx+si+1111h]
     
-    adc     al,bh
-    adc     cl,11h
+    adc     ax,ax;13op
+    adc     cx,dx
+    adc     cx,ax
+    adc     ax,cx
+    adc     ax,es:[bx+si]
+    adc     ax,es:[bx+si+11h]
+    adc     ax,es:[bx+si+1111h]
+    adc     eax,es:[ebx+esi]
+    adc     ecx,fs:[ebx+esi+11h]
+    adc     edx,gs:[ebx+esi+1111h]
+    adc     ebx,cs:[ebx+esi+11111111h]
+    
+    adc     al,11h;14op
+    
+    adc     ax,1111h;15op
+    adc     eax,11111111h
+    
+    adc     cl,11h;80op
     adc     dl,11h
     adc     bl,11h
     adc     ah,11h
     adc     ch,11h
     adc     dh,11h
     adc     bh,11h
+    adc     byte ptr ds:[bx+si],11h
+    adc     byte ptr fs:[bx+si+11h],11h
+    adc     byte ptr es:[bx+si+1111h],11h
 
-
-    adc     al,11h;82op
-    adc     cl,11h
-    adc     dl,11h
-    adc     bl,11h
-    adc     ah,11h
-    adc     ch,11h
-    adc     dh,11h
-    adc     bh,11h
+    adc     cx,1111h;81op
+    adc     dx,1111h
+    adc     bx,1111h
+    adc     sp,1111h
+    adc     bp,1111h
+    adc     si,1111h
+    adc     di,1111h
+    adc     word ptr ds:[bx+si],1111h
+    adc     word ptr fs:[bx+si+11h],1111h
+    adc     word ptr es:[bx+si+1111h],1111h
+    adc     word ptr ds:[ebx+esi],1111h
+    adc     word ptr fs:[ebx+esi+11h],1111h
+    adc     word ptr es:[ebx+esi+1111h],1111h
+    adc     dword ptr gs:[bx+si],1111h
+    adc     dword ptr fs:[bx+si+11h],11111111h
+    adc     dword ptr ds:[bx+si+1111h],11116543h
+    adc     dword ptr gs:[ebx+esi],1111h
+    adc     dword ptr fs:[ebx+esi+11h],11111111h
+    adc     dword ptr ds:[ebx+esi+1111h],11116543h
     
+    ;82 est' li on?
+    
+    adc     cx,11h;83op
+    adc     dx,11h
+    adc     bx,11h
+    adc     sp,11h
+    adc     bp,11h
+    adc     si,11h
+    adc     di,11h
     adc     eax,11h
     adc     ecx,11h
     adc     edx,11h
@@ -64,42 +116,19 @@ Start:
     adc     ebp,11h
     adc     esi,11h
     adc     edi,11h
-    
-    adc     ax,1111h
-    adc     cx,1111h
-    adc     dx,1111h
-    adc     bx,1111h
-    adc     sp,1111h
-    adc     bp,1111h
-    adc     si,1111h
-    adc     di,1111h
-    
-    adc     eax,1111h
-    adc     ecx,1111h
-    adc     edx,1111h
-    adc     ebx,1111h
-    adc     esp,1111h
-    adc     ebp,1111h
-    adc     esi,1111h
-    adc     edi,1111h
-    
-    adc     es:[bx+si],11h
-    adc     cs:[si+11h],11h
-    adc     ss:[bx+1111h],1111h
-    adc     ax,1111h
-    adc     al,11h
-    adc     ds:[bx+si],ah
-    adc     es:[si+11h],cx
-    adc     cs:[bp+di+1111h],bl
-    adc     a,bh
-    adc     ax,bx
-    adc     ch,dl
-    adc     ax,ss:[bx+si]
-    adc     bp,es:[si+1111h]
-    adc     sp,cs:[bp+11h]
-    adc     ah,a
-    
-    end Start
+    adc     word ptr ds:[bx+si],11h
+    adc     word ptr fs:[bx+si+11h],11h
+    adc     word ptr es:[bx+si+1111h],11h
+    adc     word ptr ds:[ebx+esi],11h
+    adc     word ptr fs:[ebx+esi+11h],11h
+    adc     word ptr es:[ebx+esi+1111h],11h
+    adc     dword ptr gs:[bx+si],11h
+    adc     dword ptr fs:[bx+si+11h],11h
+    adc     dword ptr ds:[bx+si+1111h],11h
+    adc     dword ptr gs:[ebx+esi],11h
+    adc     dword ptr fs:[ebx+esi+11h],11h
+    adc     dword ptr ds:[ebx+esi+1111h],11h
+    end     Start
     
     
     
